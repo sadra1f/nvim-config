7fe2622b1e01047dd8b65930aebf0c858a506656
Sadra Saderi <sadrassx@gmail.com>
2025-12-16 09:30:51 +0330

    Increase treesitter max file size limit to 200kb

--- lua/config/lazy/treesitter.lua
+++ lua/config/lazy/treesitter.lua
@@ -38,11 +38,11 @@ return {
 							return true
 						end
 
-						local max_filesize = 100 * 1024 -- 100 KB
+						local max_filesize = 200 * 1024 -- 200 KB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
 							vim.notify(
-								"File larger than 100KB treesitter disabled for performance",
+								"File larger than 200KB treesitter disabled for performance",
 								vim.log.levels.WARN,
 								{ title = "Treesitter" }
 							)
