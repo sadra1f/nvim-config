952b65af70aa35d89b7e4a7eff051c731d8c9b98
Sadra Saderi <sadrassx@gmail.com>
2025-12-27 16:18:39 +0330

    Improve lualine blame

--- lua/config/lazy/lualine.lua
+++ lua/config/lazy/lualine.lua
@@ -2,22 +2,60 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local last_buf = nil
+		local last_line = nil
+		local last_result = ""
+
 		local blame = function()
-			local file_path = vim.fn.expand("%:p")
+			local current_buffer = vim.api.nvim_get_current_buf()
 			local current_line = vim.fn.line(".")
 
-			return vim.fn.system(
-				"git blame -fL "
-					.. current_line
-					.. ","
-					.. current_line
-					.. " "
-					.. file_path
-					.. " 2>/dev/null"
-					.. " | cut -d ' ' -f 3-5"
-					.. " | cut -c 2-"
-					.. " | tr -d '\n'"
+			-- Return cached result if nothing changed
+			if current_buffer == last_buf and current_line == last_line then
+				return last_result
+			end
+
+			last_buf = current_buffer
+			last_line = current_line
+
+			local file_path = vim.fn.expand("%:p")
+			if file_path == "" then
+				last_result = ""
+				return last_result
+			end
+
+			local cmd = string.format(
+				"git blame --line-porcelain -L %d,%d %s 2>/dev/null",
+				current_line,
+				current_line,
+				vim.fn.shellescape(file_path)
 			)
+
+			local output = vim.fn.systemlist(cmd)
+			if vim.v.shell_error ~= 0 or not output then
+				last_result = ""
+				return last_result
+			end
+
+			local author, author_time
+
+			for _, l in ipairs(output) do
+				if vim.startswith(l, "author ") then
+					author = l:sub(8)
+				elseif vim.startswith(l, "author-time ") then
+					author_time = tonumber(l:sub(13))
+				end
+			end
+
+			if not author or not author_time then
+				last_result = ""
+				return last_result
+			end
+
+			local date = os.date("%Y-%m-%d", author_time)
+			last_result = string.format("%s %s", author, date)
+
+			return last_result
 		end
 
 		require("lualine").setup({
