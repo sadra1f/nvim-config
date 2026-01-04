61dc8e203018d10b09ddd1888d99ef0898fbe30e
Sadra Saderi <sadrassx@gmail.com>
2026-01-04 15:54:38 +0330

    Improve spectre config

--- lua/config/lazy/spectre.lua
+++ lua/config/lazy/spectre.lua
@@ -4,17 +4,17 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		-- vim.keymap.set("n", "<leader>pwh", '<cmd>lua require("spectre").toggle()<CR>', {
-		-- 	desc = "Toggle Spectre",
-		-- })
-		vim.keymap.set("n", "<leader>pwf", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-			desc = "Search current word",
+		local spectre = require("spectre")
+
+		spectre.setup({
+			open_cmd = "rightbelow vnew",
 		})
-		vim.keymap.set("v", "<leader>pwf", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-			desc = "Search current word",
-		})
-		vim.keymap.set("n", "<leader>pcf", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-			desc = "Search on current file",
-		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>pwf", function()
+			spectre.open_visual({ select_word = true })
+		end, { desc = "Search current word" })
+		vim.keymap.set({ "n", "v" }, "<leader>pcf", function()
+			spectre.open_file_search({ select_word = true })
+		end, { desc = "Search on current file" })
 	end,
 }
