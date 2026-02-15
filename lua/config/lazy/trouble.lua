300ecac303e1e2d52b51479fa519c8900e8c498f
Sadra Saderi <sadrassx@gmail.com>
2026-02-15 16:07:02 +0330

    Add todo comments for trouble and improve config

--- lua/config/lazy/trouble.lua
+++ lua/config/lazy/trouble.lua
@@ -1,26 +1,48 @@
 return {
 	{
+		"folke/todo-comments.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			gui_style = {
+				fg = "NONE",
+				bg = "NONE",
+			},
+			highlight = {
+				before = "",
+				keyword = "bg",
+				after = "",
+			},
+		},
+	},
+	{
 		"folke/trouble.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"folke/todo-comments.nvim",
+		},
+		opts = {
+			modes = {
+				workspace = {
+					desc = "Diagnostics and TODOs",
+					sections = {
+						"diagnostics",
+						"todo",
+					},
+				},
+			},
+			win = {
+				position = "right",
+				size = 40,
+			},
+		},
 		cmd = "Trouble",
 		keys = {
 			{
 				"<leader>vt",
 				function()
-					require("trouble").toggle("diagnostics")
-				end,
-			},
-
-			{
-				"[t",
-				function()
-					require("trouble").next("diagnostics", { skip_groups = true, jump = true })
-				end,
-			},
-
-			{
-				"]t",
-				function()
-					require("trouble").previous("diagnostics", { skip_groups = true, jump = true })
+					require("trouble").toggle("workspace")
 				end,
 			},
 		},
