f71a59e0abd9519fbb053b513dc08ecad15d8cf2
Sadra Saderi <sadrassx@gmail.com>
2026-07-09 12:40:14 +0330

    Add guess-indent

--- lua/config/lazy/indent.lua
+++ lua/config/lazy/indent.lua
@@ -1,12 +1,19 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-	opts = {
-		scope = {
-			show_start = false,
-			show_end = false,
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {
+			scope = {
+				show_start = false,
+				show_end = false,
+			},
 		},
+	},
+	{
+		"nmac427/guess-indent.nvim",
+		commit = "84a4987ff36798c2fc1169cbaff67960aed9776f",
+		opts = { auto_cmd = true },
 	},
 }
