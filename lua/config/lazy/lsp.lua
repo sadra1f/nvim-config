17a29c00ad9b765e3416ce802782aa0d26a5f41a
Sadra Saderi <sadrassx@gmail.com>
2026-08-31 17:22:08 +0330

    Add Vue LSP config

--- lua/config/lazy/lsp.lua
+++ lua/config/lazy/lsp.lua
@@ -96,6 +96,32 @@ return {
 			end,
 		})
 
+		vim.lsp.config("vtsls", {
+			filetypes = {
+				"typescript",
+				"javascript",
+				"javascriptreact",
+				"typescriptreact",
+				"vue",
+			},
+			settings = {
+				vtsls = {
+					tsserver = {
+						globalPlugins = {
+							{
+								name = "@vue/typescript-plugin",
+								location = vim.fn.stdpath("data")
+									.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+								languages = { "vue" },
+								configNamespace = "typescript",
+								enableForWorkspaceTypeScriptVersions = true,
+							},
+						},
+					},
+				},
+			},
+		})
+
 		require("fidget").setup({})
 		require("mason").setup()
 		require("mason-lspconfig").setup({
@@ -116,6 +142,7 @@ return {
 				-- "rust_analyzer",
 				"tailwindcss",
 				"vtsls",
+				"vue_ls",
 			},
 		})
 
