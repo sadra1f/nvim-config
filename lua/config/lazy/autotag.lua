5706c6099dcdae9f90b7bfcc68f31f32b8851171
Sadra Saderi <sadrassx@gmail.com>
2026-09-15 14:48:50 +0330

    Disable autotag rename globally

--- lua/config/lazy/autotag.lua
+++ lua/config/lazy/autotag.lua
@@ -6,13 +6,9 @@ return {
 				opts = {
 					-- Defaults
 					enable_close = true, -- Auto close tags
-					enable_rename = true, -- Auto rename pairs of tags
+					-- You can use `vim.lsp.buf.rename()` to rename tags
+					enable_rename = false, -- Auto rename pairs of tags
 					enable_close_on_slash = false, -- Auto close on trailing </
-				},
-				per_filetype = {
-					["vue"] = {
-						enable_rename = false,
-					},
 				},
 			})
 		end,
