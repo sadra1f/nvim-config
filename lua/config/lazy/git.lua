01aac806a1e6080c86e0d1bb3f8e2c6db7c7e72b
Sadra Saderi <sadrassx@gmail.com>
2026-09-21 11:20:37 +0330

    Improve git command and keymaps

--- lua/config/lazy/git.lua
+++ lua/config/lazy/git.lua
@@ -1,5 +1,50 @@
 return {
-	{ "tpope/vim-fugitive", cmd = { "Git" } },
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"Git",
+			"Gedit",
+			"Gsplit",
+			"Gdiffsplit",
+			"Gvdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"Glgrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+		},
+		keys = {
+			{
+				"<leader>gg",
+				"<cmd>botright vertical Git<cr>"
+					.. "<cmd>vertical resize 48<cr>"
+					.. "<cmd>set nonumber<cr>"
+					.. "<cmd>set norelativenumber<cr>",
+				mode = { "n" },
+				desc = "Git",
+			},
+			{
+				"<leader>gd",
+				"<cmd>Gvdiffsplit<cr>",
+				mode = { "n" },
+				desc = "Git diff split",
+			},
+			{
+				"<leader>gb",
+				"<cmd>Git blame<cr>",
+				mode = { "n" },
+				desc = "Git blame",
+			},
+			{
+				"<leader>gm",
+				"<cmd>Git mergetool<cr>",
+				mode = { "n" },
+				desc = "Git merge",
+			},
+		},
+	},
 	{
 		"airblade/vim-gitgutter",
 		config = function()
